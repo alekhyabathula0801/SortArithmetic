d6e32d5fe90615a52877af1caeba33c9dcb20431
arun kumar <alekhya0801@gmail.com>
2020-04-12 12:35:46 +0530

    Added storing values in array feature

--- sortArithmeticComputation.sh
+++ sortArithmeticComputation.sh
@@ -2,8 +2,8 @@
 read -p "enter first number " a
 read -p "enter second number " b
 read -p "enter third number " c
-#declare -A arithmetic
 arithmetic[1]=$(($a+$b*$c))
 arithmetic[2]=$(($a*$b+$c))
 arithmetic[3]=$(($a/$b+$c))
 arithmetic[4]=$(($a%$b+$c))
+echo ${arithmetic[@]}
