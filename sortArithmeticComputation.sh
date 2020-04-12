daf81259b8b9bf23f3681cf5b291ff23612725ba
arun kumar <alekhya0801@gmail.com>
2020-04-12 12:21:52 +0530

    Refracted code using dictionary

--- sortArithmeticComputation.sh
+++ sortArithmeticComputation.sh
@@ -2,7 +2,8 @@
 read -p "enter first number " a
 read -p "enter second number " b
 read -p "enter third number " c
-x=$(($a+$b*$c))
-y=$(($a*$b+$c))
-z=$(($a/$b+$c))
-p=$(($a%$b+$c))
+#declare -A arithmetic
+arithmetic[1]=$(($a+$b*$c))
+arithmetic[2]=$(($a*$b+$c))
+arithmetic[3]=$(($a/$b+$c))
+arithmetic[4]=$(($a%$b+$c))
