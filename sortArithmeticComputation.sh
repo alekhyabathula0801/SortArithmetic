27a712fa85e46f50bd35dbbb1a2e3510f401bbc0
arun kumar <alekhya0801@gmail.com>
2020-04-12 12:58:47 +0530

    Added feature to sort in descending order

--- sortArithmeticComputation.sh
+++ sortArithmeticComputation.sh
@@ -6,4 +6,17 @@ arithmetic[1]=$(($a+$b*$c))
 arithmetic[2]=$(($a*$b+$c))
 arithmetic[3]=$(($a/$b+$c))
 arithmetic[4]=$(($a%$b+$c))
+for (( i=1; i<=4; i++ ))
+do
+	for(( j=i+1; j<=4; j++ ))
+	do
+		if [ ${arithmetic[j]} -gt ${arithmetic[i]} ]
+		then
+			temp=${arithmetic[j]}
+			arithmetic[j]=${arithmetic[i]}
+			arithmetic[i]=$temp
+		fi
+	done
+done
 echo ${arithmetic[@]}
+
